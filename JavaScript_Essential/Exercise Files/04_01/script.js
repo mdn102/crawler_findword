38de809a8155d8f105f99def1a9377f338977f27
nguyendm77 <nguyendm77@gmail.com>
2021-01-12 11:02:50 -0800

    saved

--- JavaScript_Essential/Exercise Files/04_01/script.js
+++ JavaScript_Essential/Exercise Files/04_01/script.js
@@ -7,7 +7,7 @@ multiply();
 
 // Anonymous function stored in variable.
 // Invoked by calling the variable as a function:
-var divided = function() {
+var divided = function () {
     var result = 3 / 4;
     console.log("3 divided by 4 is ", result);
 }
@@ -15,7 +15,7 @@ divided();
 
 // Immediately Invoked Function Expression.
 // Runs as soon as the browser finds it:
-(function() {
+(function () {
     var result = 12 / 0.75;
     console.log("12 divided by 0.75 is ", result);
 }())
