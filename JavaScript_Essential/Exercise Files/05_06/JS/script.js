b4e27b86d69d3797b196d3b45d3c2b9dbb3e1bfa
nguyendm77 <nguyendm77@gmail.com>
2021-01-12 14:39:20 -0800

    saved

--- JavaScript_Essential/Exercise Files/05_06/JS/script.js
+++ JavaScript_Essential/Exercise Files/05_06/JS/script.js
@@ -1,1 +1,20 @@
+const FEATURED = document.querySelector(".featured-image");
+const THEIMAGE = FEATURED.querySelector("img");
 
+
+var altText = THEIMAGE.getAttribute("alt");
+
+var captionElement = document.createElement("figcaption");
+
+var captionText = document.createTextNode(altText);
+
+captionElement.appendChild(captionText);
+
+//  console.log(captionElement);
+
+captionElement.append(altText);
+FEATURED.append(captionElement);
+// FEATURED.appendChild(captionElement);
+
+
+THEIMAGE.setAttribute("alt", "");
