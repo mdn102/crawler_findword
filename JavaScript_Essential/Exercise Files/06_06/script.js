6dec975eaaa6483b63a46de601de827db101ff4f
nguyendm77 <nguyendm77@gmail.com>
2021-01-12 16:04:39 -0800

    saved

--- JavaScript_Essential/Exercise Files/06_06/script.js
+++ JavaScript_Essential/Exercise Files/06_06/script.js
@@ -9,9 +9,9 @@ let min = date.getMinutes();
 let sec = date.getSeconds();
 console.log("Hour: " + hr + " Minute: " + min + " Second: " + sec);
 
-let hrPosition = (hr*360/12)+(min*(360/60)/12);
-let minPosition = (min*360/60)+(sec*(360/60)/60);
-let secPosition = sec*360/60;
+let hrPosition = (hr * 360 / 12) + (min * (360 / 60) / 12);
+let minPosition = (min * 360 / 60) + (sec * (360 / 60) / 60);
+let secPosition = sec * 360 / 60;
 
 HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
 MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
--- JavaScript_Essential/Exercise Files/06_07/script.js
+++ JavaScript_Essential/Exercise Files/06_07/script.js
@@ -10,9 +10,9 @@ function runTheClock() {
     let sec = date.getSeconds();
     console.log("Hour: " + hr + " Minute: " + min + " Second: " + sec);
 
-    let hrPosition = (hr*360/12)+(min*(360/60)/12);
-    let minPosition = (min*360/60)+(sec*(360/60)/60);
-    let secPosition = sec*360/60;
+    let hrPosition = (hr * 360 / 12) + (min * (360 / 60) / 12);
+    let minPosition = (min * 360 / 60) + (sec * (360 / 60) / 60);
+    let secPosition = sec * 360 / 60;
 
     HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
     MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
