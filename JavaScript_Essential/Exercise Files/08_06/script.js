4b0ba79ac858284bdf65c08d9b0170394629ba14
nguyendm77 <nguyendm77@gmail.com>
2021-01-12 16:57:21 -0800

    saved

--- JavaScript_Essential/Exercise Files/08_06/script.js
+++ JavaScript_Essential/Exercise Files/08_06/script.js
@@ -4,7 +4,7 @@ const originText = document.querySelector("#origin-text p").innerHTML;
 const resetButton = document.querySelector("#reset");
 const theTimer = document.querySelector(".timer");
 
-var timer = [0,0,0,0];
+var timer = [0, 0, 0, 0];
 
 // Add leading zero to numbers 9 or below (purely for aesthetics):
 function leadingZero(time) {
@@ -20,8 +20,8 @@ function runTimer() {
     theTimer.innerHTML = currentTime;
     timer[3]++;
 
-    timer[0] = Math.floor((timer[3]/100)/60);
-    timer[1] = Math.floor((timer[3]/100) - (timer[0] * 60));
+    timer[0] = Math.floor((timer[3] / 100) / 60);
+    timer[1] = Math.floor((timer[3] / 100) - (timer[0] * 60));
     timer[2] = Math.floor(timer[3] - (timer[1] * 100) - (timer[0] * 6000));
 }
 
