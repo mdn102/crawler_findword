0187cf984a6fbe5ecf44c50dd49b7af4b8bfd143
nguyendm77 <nguyendm77@gmail.com>
2021-01-12 21:08:15 -0800

    saved

--- 30js-challenge/challenge1/fibonacci.js
+++ 30js-challenge/challenge1/fibonacci.js
@@ -1,8 +1,8 @@
-//Using recursive function//
-var fibonacci = function(n){
-    return n < 2 ? n : fibonacci(n-1) + fibonacci(n-2);
+// Using recursive function//
+var fibonacci = function (n) {
+  return n < 2 ? n : fibonacci(n - 1) + fibonacci(n - 2)
 }
 
-for (let i = 0; i<=10; i++){
-    console.log(`${(i)} : ${fibonacci(i)}`)
+for (let i = 0; i <= 10; i++) {
+  console.log(`${(i)} : ${fibonacci(i)}`)
 }
