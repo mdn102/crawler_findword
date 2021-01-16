d8ac490563b0667eab0597b37f74c0c6d77b9371
nguyendm77 <nguyendm77@gmail.com>
2021-01-16 09:33:33 -0800

    updated code

--- Learning_Python/Exercise Files/Ch3/dates_start.py
+++ Learning_Python/Exercise Files/Ch3/dates_start.py
@@ -1,15 +1,18 @@
 #
 # Example file for working with date information
 #
-
+from datetime import date 
+from datetime import time
+from datetime import datetime
 
 def main():
   ## DATE OBJECTS
   # Get today's date from the simple today() method from the date class
-
+today = date.today()
+print("Today's date is ", today)
 
   # print out the date's individual components
-
+print("Date components: ", today.day, today.month, today.year)
   
   # retrieve today's weekday (0=Monday, 6=Sunday)
 
