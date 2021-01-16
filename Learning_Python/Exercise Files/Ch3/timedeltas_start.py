d83025250de537b9cbf0e2b717d34c45d7809ee2
nguyendm77 <nguyendm77@gmail.com>
2021-01-16 09:50:36 -0800

    updated code

--- Learning_Python/Exercise Files/Ch3/timedeltas_start.py
+++ Learning_Python/Exercise Files/Ch3/timedeltas_start.py
@@ -5,13 +5,15 @@
 from datetime import date
 from datetime import time
 from datetime import datetime
+from datetime import timedelta
 
 
 # construct a basic timedelta and print it
-
+print(timedelta(days=365, hours=5, minutes=1))
 
 # print today's date
-
+now = datetime.now()
+print("today is: " + str(now))
 
 # print today's date one year from now
 
