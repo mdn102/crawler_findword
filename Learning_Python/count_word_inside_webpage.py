2959f4cf2d2b0b44e7bd705c498bc9f911f857a1
nguyendm77 <nguyendm77@gmail.com>
2021-01-18 20:20:33 -0800

    saved

---
+++ Learning_Python/count_word_inside_webpage.py
@@ -0,0 +1,20 @@
+import requests
+from bs4 import BeautifulSoup
+from collections import Counter
+from string import punctuation
+
+# We get the url
+r = requests.get("https://en.wikiquote.org/wiki/Khalil_Gibran")
+soup = BeautifulSoup(r.content)
+
+# We get the words within paragrphs
+text_p = (''.join(s.findAll(text=True))for s in soup.findAll('p'))
+c_p = Counter((x.rstrip(punctuation).lower() for y in text_p for x in y.split()))
+
+# We get the words within divs
+text_div = (''.join(s.findAll(text=True))for s in soup.findAll('div'))
+c_div = Counter((x.rstrip(punctuation).lower() for y in text_div for x in y.split()))
+
+# We sum the two countesr and get a list with words count from most to less common
+total = c_div + c_p
+list_most_common_words = total.most_common() 
