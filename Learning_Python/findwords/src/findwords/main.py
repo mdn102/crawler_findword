850617431950ace6559182a961c4e4fcf020909c
nguyendm77 <nguyendm77@gmail.com>
2021-01-19 22:17:46 -0800

    saved

--- Learning_Python/findwords/src/findwords/main.py
+++ Learning_Python/findwords/src/findwords/main.py
@@ -1,9 +1,11 @@
-import sys, getopt
+import sys
+import getopt
 import urllib.request
 from urllib.request import urlopen
 import ssl
 
 ssl._create_default_https_context = ssl._create_unverified_context
+
 
 def crawler(url):
     content = url
@@ -15,12 +17,12 @@ if __name__ == '__main__':
     NUM_WORDS = sys.argv[1]
     EXCLUDE_WORDS = sys.argv[2:]
 
-    print (f"Print total {NUM_WORDS} words")
-    print (f"Words that are excluded: {EXCLUDE_WORDS}")
-    
+    print(f"Print total {NUM_WORDS} words")
+    print(f"Words that are excluded: {EXCLUDE_WORDS}")
+
     content = crawler("https://en.wikipedia.org/wiki/Microsoft")
     print(content)
-    
+
     # read the data from the URL and print it
     file = urlopen(content)
     data = file.read()
