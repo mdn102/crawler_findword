b81a05b8a69ff8b5819adfdd6d24bbd369a45acf
nguyendm77 <nguyendm77@gmail.com>
2021-01-19 20:27:49 -0800

    saved

--- Learning_Python/findwords/src/findwords/main.py
+++ Learning_Python/findwords/src/findwords/main.py
@@ -1,4 +1,13 @@
 import sys, getopt
+import requests
+# from bs4 import BeautifulSoup
+from collections import Counter
+from string import punctuation
+
+def crawler(url):
+    content = url
+    return content 
+    
 
 if __name__ == '__main__':
     # Parsing argument
@@ -7,3 +16,9 @@ if __name__ == '__main__':
 
     print (f"Print total {NUM_WORDS} words")
     print (f"Words that are excluded: {EXCLUDE_WORDS}")
+    content = crawler("https://en.wikipedia.org/wiki/Microsoft")
+    print(content)
+    
+    r = requests.get("https://en.wikipedia.org/wiki/Microsoft")
+    # soup = BeautifulSoup(r.content)
+    text = (''.join(s.findAll(text=True))for s in soup.findAll('p'))
