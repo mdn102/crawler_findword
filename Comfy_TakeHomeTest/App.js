baca1e297de4befa976808e157ed037ebaabc61c
nguyendm77 <nguyendm77@gmail.com>
2021-01-22 20:46:52 -0800

    changed

--- Comfy_TakeHomeTest/App.js
+++ Comfy_TakeHomeTest/App.js
@@ -31,4 +31,4 @@ function countWords(str) {
 // }
 
 let str = "Anyway, I keep picturing all these little kids playing some game in this big field of rye and all. Thousands of little kids, and nobody's around - nobody big, I mean - except me. And I'm standing on the edge of some crazy cliff. What I have to do, I have to catch everybody if they start to go over the cliff - I mean if they're running and they don't look where they're going I have to come out from somewhere and catch them. That's all I do all day. I'd just be the catcher in the rye and all. I know it's crazy, but that's the only thing I'd really like to be. "
-wordCounts(str);
+countWords(str);
